fd85d58899ca1c0594097c1f27b597e18af13b5c
Diego165418 <00165418@uca.edu.sv>
2024-08-25 21:39:50 -0600

    :hammer: modificando User

--- tasks/models.py
+++ tasks/models.py
@@ -1,27 +1,50 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.utils import timezone
 
 # Create your models here.
-class UserRole (models.Model):
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not username:
+            raise ValueError('The Username field must be set')
+        if not email:
+            raise ValueError('The Email field must be set')
+
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(username, email, password, **extra_fields)
+class UserRole(models.Model):
     role_name = models.CharField(max_length=20, unique=True)
 
     def __str__(self):
         return self.role_name
-    
-class User(AbstractBaseUser):
+
+class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=50, unique=True)
     email = models.EmailField(max_length=100, unique=True)
     password = models.CharField(max_length=255)
     role = models.ForeignKey(UserRole, on_delete=models.SET_NULL, null=True, blank=True)
     date_joined = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)  # Necesario para el admin
 
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['email']
 
+    objects = UserManager()
+
     def __str__(self):
         return self.username
+
+    @classmethod
+    def get_by_natural_key(cls, username):
+        return cls.objects.get(username=username)
     
 class Event(models.Model):
     name = models.CharField(max_length=100)
