ada83603d9740c23da150a756d5ca9d6160839e6
Diego165418 <00165418@uca.edu.sv>
2024-08-25 23:17:50 -0600

    Modificando para archivos estaticos

--- event_management/urls.py
+++ event_management/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from tasks.views import view_home
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', view_home, name='home'), 
     path('', include('tasks.urls'))
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
